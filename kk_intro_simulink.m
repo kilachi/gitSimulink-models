2e43ec046fab2e2f4021903836dad1e5c266017c
koreykam <koreykam@gmail.com>
2014-09-21 15:15:03 -0400

    intro to sim

---
+++ kk_intro_simulink.m
@@ -0,0 +1,12 @@
+%kk_simulink script
+
+%% Display Time-Domain Data in Timescope
+ex_timescope_tut
+
+%% Display Frequency-Domain Data in Spectrum Analyzer
+ex_spectrumanalyzer_tut
+
+%% Live Image Acquisition and Histogram Display
+demoimaqsl_rgbhistogram_maccam
+
+%%
